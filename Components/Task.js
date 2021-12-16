9ff30d5ff4eff21235a7f3c090cd1590c6da3eb3
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-15 19:16:38 -0800

    Add square/leftItem styles for items

--- Components/Task.js
+++ Components/Task.js
@@ -23,20 +23,26 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         padding: 18,
         borderRadius: 11,
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between', //setting this will counter any item on right side of item box
+        marginBottom: 20,
     },
     leftItem: {
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
     },
     square: {
         width: 26,
         height: 24,
-        backgroundColor: 'gray',
+        backgroundColor: '#3267AB',
         opacity: .6,
-
+        borderRadius: 5,
+        marginRight: 15, //cannot be pushed to the right past this margin
     },
     itemText: {
-
+        
     },
     circle: {
 

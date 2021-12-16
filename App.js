85ebce99b8f9f353560adbe37ef78286a67b8d60
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-16 09:00:08 -0800

    Add task input styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
 import Task from './Components/Task';
 
 export default function App() {
@@ -27,10 +27,10 @@ export default function App() {
             behavior={Platform.OS === 'ios' ? "padding" : "height"}
             style={styles.writeWrapper}
         >
-            <TextInupt style={styles.input} placeholder={'Write a task'} />
+            <TextInput style={styles.input} placeholder={'Write a task'} />
             <TouchableOpacity >
                 <View style={styles.addWrapper}>
-                    <Text style={styles.addText}>task info</Text>
+                    <Text style={styles.addText}>+</Text>
                 </View>
             </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -39,26 +39,38 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3267AB',
-  },
-  taskWrapper: {
-    paddingTop: 100,
-    paddingHorizontal: 20,
-  },
-  section: { //title
-    fontSize: 32,
-    fontWeight: '600',
-  },
-  items: {
-    marginTop: 25,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#3267AB',
+    },
+    taskWrapper: {
+        paddingTop: 100,
+        paddingHorizontal: 20,
+    },
+    section: { //title
+        fontSize: 32,
+        fontWeight: '600',
+    },
+    items: {
+        marginTop: 25,
+    },
     writeWrapper: {
-
+        position: 'absolute', 
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between', //will push left item left, right item right
+        alignItems: 'center',
     },
     input: {
-
+        paddingVertical: 22,
+        paddingHorizontal: 15,
+        width: 250,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        borderColor: '#3267AB',
+        borderWidth: 3,
+        
     },
     addWrapper: {
 
